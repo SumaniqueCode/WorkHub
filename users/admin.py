b4d59031bbf0813676829d4514fad1fa2f7c1775
Suman Regmi <limitlessplaying@gmail.com>
2026-01-27 19:07:44 +0545

    integrated models in admin

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
 from .models import *
+from skills.models import Skill
+
+class ExperienceInline(admin.StackedInline):
+    model = Experience
+    extra = 1
+class EducationInline(admin.StackedInline):
+    model = Education
+    extra = 1
+class CertificationInline(admin.StackedInline):
+    model = Certification
+    extra = 1   
+class SocialLinkInline(admin.StackedInline):
+    model = SocialLink
+    extra = 1
+class ProjectInline(admin.StackedInline):
+    model = Projects
+    extra = 1
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['id', 'user']
+    inlines = [ExperienceInline, EducationInline, CertificationInline, SocialLinkInline, ProjectInline]  
     
-    
-# Register your models here.
 admin.site.register(Profile, ProfileAdmin)
