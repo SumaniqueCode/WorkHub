a81b9eb1c1592099b1b5b42739fc6723a26ee57a
SumaniqueCode <github.regmisuman@gmail.com>
2026-03-14 12:05:16 +0545

    main view optimized for recommended jobs

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from users.models import Profile
+from users.recommendation import recommend_jobs
 
 
 def landingPage(request):
@@ -7,4 +9,13 @@ def landingPage(request):
 
 @login_required(login_url='/user/login')
 def dashboard(request):
-    return render(request, 'pages/dashboard/jdashboard.html')
+    try:
+        profile = request.user.profile
+        recommended_jobs = recommend_jobs(profile, top_n=5)
+    except Profile.DoesNotExist:
+        recommended_jobs = []
+        
+    context = {
+        'recommended_jobs': recommended_jobs
+    }
+    return render(request, 'pages/dashboard/jdashboard.html', context)
