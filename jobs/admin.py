bc114b370d004756354a2670e7a9516ad594c536
SumaniqueCode <github.regmisuman@gmail.com>
2026-03-27 12:37:17 +0545

    Fields added in the admin of jobs

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
+from unfold.decorators import action
+from django.urls import reverse_lazy
 from .models import Job
 
-class JobAdmin(admin.ModelAdmin):
-    list_display = ("title", "company", "recruiter", "location", "employment_type", "work_mode", "created_at")
-    search_fields = ("title", "company", "location", "recruiter__username")
-    list_filter = ("employment_type", "work_mode", "created_at")
+class JobAdmin(ModelAdmin):
+    list_display = ("title", "company", "recruiter", "location", "employment_type", "work_mode", "created_at", "is_active")
+    search_fields = ("title", "company__name", "location", "recruiter__username")
+    list_filter = ("employment_type", "work_mode", "created_at", "is_active")
     ordering = ("-created_at",)
     
 admin.site.register(Job, JobAdmin)
-
 
