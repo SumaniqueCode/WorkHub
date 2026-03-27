49c12dfb1697cfed4089e8aec38a820654de101d
SumaniqueCode <github.regmisuman@gmail.com>
2026-03-27 12:36:27 +0545

    Fields added in the admin of users

--- users/admin.py
+++ users/admin.py
@@ -1,25 +1,42 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin, StackedInline
+from unfold.decorators import action
 from .models import *
 from skills.models import Skill
 
-class ExperienceInline(admin.StackedInline):
+class ExperienceInline(StackedInline):
     model = Experience
     extra = 1
-class EducationInline(admin.StackedInline):
+
+class EducationInline(StackedInline):
     model = Education
     extra = 1
-class CertificationInline(admin.StackedInline):
+
+class CertificationInline(StackedInline):
     model = Certification
     extra = 1   
-class SocialLinkInline(admin.StackedInline):
+
+class SocialLinkInline(StackedInline):
     model = SocialLink
     extra = 1
-class ProjectInline(admin.StackedInline):
+
+class ProjectInline(StackedInline):
     model = Project
     extra = 1
 
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user']
+class ProfileAdmin(ModelAdmin):
+    list_display = ['id', 'user', 'role', 'position', 'address']
+    list_filter = ['role', 'gender', 'preferred_work_mode', 'preferred_job_type']
+    search_fields = ['user__username', 'user__email', 'position', 'address']
     inlines = [ExperienceInline, EducationInline, CertificationInline, SocialLinkInline, ProjectInline]  
-    
+
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+admin.site.unregister(User)
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    pass
+
 admin.site.register(Profile, ProfileAdmin)
